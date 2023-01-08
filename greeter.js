86fc190b26094001ec4c5692e4cee3f83b10b36a
toriumi72 <106057295+toriumi72@users.noreply.github.com>
2023-01-08 21:19:11 +0900

    first commit

---
+++ greeter.js
@@ -0,0 +1,6 @@
+"use strict";
+function greeter(person) {
+    return "hello," + person;
+}
+let user = "jane Doe";
+document.body.textContent = greeter(user);
---
+++ greeter.ts
@@ -0,0 +1,8 @@
+function greeter(person: string) {
+    return "hello," + person;
+}
+
+let user = "jane Doe";
+
+document.body.textContent = greeter(user);
+
